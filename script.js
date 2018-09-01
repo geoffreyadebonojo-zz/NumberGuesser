32e8ba3e8b906bf476faeb1d4170273dfa2a4a7c
Geoffrey Adebonojo <geoffrey.adebonojo@gmail.com>
2018-09-01 13:37:00 -0600

    expand feedback options, refactor methods, optimize passive aggression

--- script.js
+++ script.js
@@ -1,27 +1,59 @@
 
 var NUMBER = Math.round(Math.random()*100)
 var inputEl = document.querySelector('.input');
-var answer = "nothing yet"
+var answer = "I'm sure it's fine"
 var answerField = document.querySelector('.answer');
 answerField.innerHTML = answer;
+var numGuesses = 0
+var gentleEncouragingWords = document.querySelector('.gentleencouragement');
 
 //GUESS BOTTON
 var guessButton = document.querySelector('.guessbutton');
 guessButton.addEventListener('click', guessCheck);
 function guessCheck () {
-	console.log("Guess = " + inputEl.value);
+	numGuesses += 1;
+	console.log("Guess :" + " " + numGuesses + " => " + inputEl.value);
 	console.log("Number = " + NUMBER);
 	if (inputEl.value == NUMBER) {
 	console.log("Correct!")
-	answerField.innerHTML = "Correct!"	
-	} else if (Math.abs(inputEl.value - NUMBER)<10) {
-	console.log("close!");
-	answerField.innerHTML = "Close!"
+	answerField.innerHTML = "Correct! I never thought you had it in you. It's probably luck."	
+	//are you cheating??
+	} else if ((inputEl.value - NUMBER)>0 && (inputEl.value - NUMBER)<10){
+	console.log("a little too high...");
+	answerField.innerHTML = "Maybe be less aggressive"
+	
+	} else if ((inputEl.value - NUMBER)>9 && (inputEl.value - NUMBER)<30){
+	console.log("Way High!!");
+	answerField.innerHTML = "nothing's Wrong. nothing at all."
+	
+	} else if ((inputEl.value - NUMBER)<0 && (inputEl.value - NUMBER)>-10){
+	console.log("a little too low...")
+	answerField.innerHTML = "You're not being confident enough!"
+	
+	} else if ((inputEl.value - NUMBER)<-9 && (inputEl.value - NUMBER)>-30){
+	console.log("Way Low!!");
+	answerField.innerHTML = "Nothing's wrong. I mean, it's fine.";
 	}
-	else {
-	console.log("Incorrect.");
-	answerField.innerHTML = "Wrong!!"
+	
+	if (numGuesses > 5 && numGuesses < 10) {
+	console.log(numGuesses)	
+	gentleEncouragingWords.innerHTML = "Wow, I can't believe you haven't gotten it yet!";
+	} else if (numGuesses >= 10 && numGuesses < 15) {
+	console.log(numGuesses)	
+	gentleEncouragingWords.innerHTML = "Jeez maybe you ought to give up..."
+	} else if (numGuesses >= 15 && numGuesses < 18) {
+	console.log(numGuesses)	
+	gentleEncouragingWords.innerHTML = "Haha..."
+	} else if (numGuesses >= 18 && numGuesses < 20) {
+	console.log(numGuesses)	
+	gentleEncouragingWords.innerHTML = "Hahahahah...!"
+	} else if (numGuesses >= 20 && numGuesses < 25) {
+	console.log(numGuesses)	
+	gentleEncouragingWords.innerHTML = "Hahahahahahaha...!!"
+	} else if (numGuesses >= 25) {
+	gentleEncouragingWords.innerHTML = "the answer is " + NUMBER + ". This stopped being funny a while ago."; 
 	}
+	
 };
 
 //DISPLAY RESULT
@@ -34,12 +66,21 @@ clearButton.addEventListener('click', clearCheck);
 function clearCheck () {
 	inputEl.value = "";
 	console.log("Clear");
+	gentleEncouragingWords.innerHTML = "Sure. Clear."
+	answerField.innerHTML = ""
 }
 
 var resetButton = document.querySelector('.reset');
 resetButton.addEventListener('click', newNumber);
 function newNumber () {
-	var NUMBER = Math.round(Math.random()*100);
+	console.log("Reset")
+	var NEW_NUMBER = Math.round(Math.random()*100);
+	NUMBER = NEW_NUMBER;
+	gentleEncouragingWords.innerHTML = "If you didn't get it then, why would you get it now?"
+	answerField.innerHTML = "You want to try again? Why?";
+	numGuesses = 0;
+	guessCheck();
+	
 }
 
 
